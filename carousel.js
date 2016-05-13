66877a2e4f7761bc3367a9e81f00730c417d512c
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-05-13 19:38:01 +0100

    Adding functionality to the arrows

--- carousel.js
+++ carousel.js
@@ -1,7 +1,8 @@
 var previousImage = '';
-var images = document.getElementsByTagName('a'); 
+var images = document.getElementsByTagName('img'); 
 
-centerImage(images[0]);
+// Center first image loaded (images[0] = left-arrow.png)
+centerImage(images[1]);
 
 function centerImage(image) {
 	var innerLeft = image.offsetLeft;
@@ -22,15 +23,52 @@ function centerImage(image) {
 
 function animation() {
 	document.getElementById('carousel-images').style.WebkitTransition = 'left 1s ease-in-out';
-	document.getElementById('carousel-images').style.transition = 'left s ease-in-out';
+	document.getElementById('carousel-images').style.transition = 'left 1s ease-in-out';
 }
 
 function changeOpacity(image) {	
 	if(previousImage != '') {
-		previousImage.childNodes[0].classList.remove('active');
+		previousImage.classList.remove('active');
 	}
 	
-	image.childNodes[0].className = 'active';
+	image.className = 'active';
 	
 	previousImage = image;
+}
+
+function changeMainImage(arrow_position) {
+	var i = currentImageIndex = 0;
+	
+	while(i < images.length) {
+		if(images[i].className == 'active') {
+			currentImageIndex = i;
+			break;
+		}
+		
+		else {
+			i++;
+		}
+	}
+	
+	if(arrow_position == 'left') {
+		// If the previous image is not the left arrow (images[0]), the new main image is the previous of the current one
+		if(currentImageIndex-1 != 0) {
+			centerImage(images[currentImageIndex-1]);
+		}
+		// If previous image is the left arrow, the new main image is the last one
+		else {
+			centerImage(images[images.length-2]);
+		}
+	}
+	
+	else {
+		// If the next image is not the right arrow (images[lenght-1]), the new main image is the next of the current one
+		if(currentImageIndex+1 != images.length-1) {
+			centerImage(images[currentImageIndex+1]);
+		}
+		// If the next image is the right arrow, the new main image is the first one
+		else {
+			centerImage(images[1]);
+		}
+	}
 }
